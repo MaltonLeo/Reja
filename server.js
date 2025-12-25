669bf3a8f7aab8ce6260ea340281393e362aa8c9
MaltonLeo <your_github_khudaynazrovs@gmail.com>
2025-12-25 09:36:07 +0900

    Modified

--- server.js
+++ server.js
@@ -20,16 +20,12 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 Routing Codes
-app.get("/hello", function(req, res){
-    res.end(`<h1 style="background:red">Hello World Its me</h1>`)
-});
-
-app.get("/gift", function(req, res) {
-    res.end(`<h1>Siz sovg'alar bo'limidasiz</h1>`)
-}); 
-
-app.get("/send", function(req, res) {
-    res.end(`<h1>Siz jo'natilganlar bo'limidasiz</h1>`)
+app.post("/create-item", (req, res) => {
+    console.log(req.body);
+    res.json({test:"success"})
+})
+app.get('/', function(req ,res){
+    res.render('harid')
 });
  const server = http.createServer(app);
  let PORT = 3000;
