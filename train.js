e8e02a3ae2ba4cba385925af42e877e5f94ea81b
MaltonLeo <your_github_khudaynazrovs@gmail.com>
2025-12-30 11:26:49 +0900

    odified

--- train.js
+++ train.js
@@ -2,13 +2,15 @@
 function countLetter(letter, word) {
     let count = 0;
     for( let i = 0; i < word.length; i++) {
+        letter = letter.toLowerCase();
+        word = word.toLowerCase();
         if(word[i] === letter) {
             count ++;
         }
     }
     return count;
 }
-console.log(countLetter('a', 'animation'))
+console.log(countLetter('A', 'animation'))
 //bunda i berilgan so'zning har birini toki so'z
 //tugaguncha olib tekshiruvchi o'zgaruvchi 
 //so'zning hsr bir harfini oladi va biz parametr sifatida
@@ -16,3 +18,5 @@ console.log(countLetter('a', 'animation'))
 //turi va qiymatini ham solishtiradi agar i va letter
 //teng bo'lsa uni hisobga oladi va keyingi harfga o'tadi toki 
 //word parametrining oxirgi harfigacha
+//Katta harflarni ham adashmasdan hisoblashi uchun ularni
+// lowerCase ga o'girib solishtiradigan qilib mantiq yozamiz
